9bc49a74ab1bd31d65ba5060dda6058f4d659780
ivan <i_nevakshenov@mail.ru>
2025-11-09 21:31:54 +0500

    add vikunja

--- 123.py
+++ 123.py
@@ -3,7 +3,7 @@ from config.config import load_config
 
 config = load_config()
 TOKEN = config.tg_bot.token
-WEB_APP_URL = "https://calc.press/"
+WEB_APP_URL = "https://miniapp.calc.press/"
 
 # Установка Menu Button
 url = f"https://api.telegram.org/bot{TOKEN}/setChatMenuButton"
