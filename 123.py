d31fcec10f45410126e050c032970b834b73004f
ivan <i_nevakshenov@mail.ru>
2025-11-24 21:35:02 +0500

    add StubAcme

--- 123.py
+++ 123.py
@@ -3,14 +3,14 @@ from config.config import load_config
 
 config = load_config()
 TOKEN = config.tg_bot.token
-WEB_APP_URL = "https://miniapp.calc.press/stub_acme_calc"
+WEB_APP_URL = "https://miniapp.calc.press/stubacme-calc"
 
 # Установка Menu Button
 url = f"https://api.telegram.org/bot{TOKEN}/setChatMenuButton"
 params = {
     "menu_button": {
         "type": "web_app",
-        "text": "🔺 Калькулятор треугольника",
+        "text": "🔺 Калькулятор резьбы",
         "web_app": {
             "url": WEB_APP_URL
         }
